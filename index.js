5d5e43bb2842b4cef41b2751cd956f9d5b4dfd77
tania <tania.190142@s.pust.ac.bd>
2025-10-18 19:10:59 +0600

    almost done my project

--- index.js
+++ index.js
@@ -36,11 +36,48 @@ var $grid = $(".grid").isotope({
 
 // ---------testimonials------------
 
+//  var swiper = new Swiper(".mySwiper", {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//       pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//       },
+//     });
+
  var swiper = new Swiper(".mySwiper", {
       slidesPerView: 3,
       spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
+      },
+      breakpoints: {
+        0: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        992: { slidesPerView: 3 },
+      },
+    });
+ var swiper = new Swiper(".mySwiperCarousel", {
+      slidesPerView: 6,
+      spaceBetween: 30,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        0: { slidesPerView: 2 },
+        768: { slidesPerView: 3 },
+        992: { slidesPerView: 5 },
       },
     });
